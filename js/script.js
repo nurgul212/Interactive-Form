c71d42f5b3873b1576a0adb072fb358ed352e568
Nurgul Amat <nurgulamat212@gmail.com>
2022-01-10 22:25:21 -0600

    added credit card field validation

--- js/script.js
+++ js/script.js
@@ -55,6 +55,7 @@ allActivities.addEventListener('change', (e) => {
     const activityCost = parseInt(selectedActivity.getAttribute('data-cost'));
 
     if (selectedActivity.checked == true) {
+        
         totalCost += activityCost;
         for (let i = 0; i < checkboxes.length; i++) {
             // Hide overlapping activities
@@ -71,7 +72,7 @@ allActivities.addEventListener('change', (e) => {
         for (let i = 0; i < checkboxes.length; i++) {
             if (selectedActivity.getAttribute('data-day-and-time') == checkboxes[i].getAttribute('data-day-and-time') ) {
                 checkboxes[i].disabled = false;
-                checkboxes[i].parentNode.classList.remove('disabled');            
+                checkboxes[i].parentNode.classList.remove('disabled');          
             }
         }
     }
@@ -138,7 +139,7 @@ function validateName(){
        return true; 
     } else {
         if(inputName.value){
-            inputName.parentNode.lastElementChild.textContent = 'Name should not contain numbers or punctuation.';
+            inputName.parentNode.lastElementChild.textContent = 'Name should not contain numbers or punctuation';
         }
         inputName.parentNode.className='not-valid';
         inputName.parentNode.lastElementChild.style.display = 'block';
@@ -156,21 +157,18 @@ inputEmail.addEventListener("keyup", () => {
 
 function validateEmail(){
      if(inputEmail.value && emailRegex.test(inputEmail.value)){
-        // inputEmail.parentNode.classList.add('valid');
         inputEmail.parentNode.className='valid';
         inputEmail.parentNode.lastElementChild.style.display = 'none';
         return true;
 
     } else {
         if(inputEmail.value){
-            inputEmail.parentNode.lastElementChild.style.display = 'block';
-            // inputEmail.nextElementSibling.textContent = 'Email address must be formatted correctly';
+            inputEmail.parentNode.lastElementChild.textContent = 'Email address must be formatted correctly';
            
         } else{
             inputEmail.parentNode.lastElementChild.textContent = 'Email address field cannot be blank';   
-            // inputEmail.nextElementSibling.textContent = 'Please enter your email';
         }
-      inputEmail.parentNode.classList.add('not-valid');
+      inputEmail.parentNode.className='not-valid';
       inputEmail.parentNode.lastElementChild.style.display = 'block';
       return false;
     }
@@ -186,11 +184,10 @@ function validateActivity(){
     for(let i = 0; i < activityList.length; i++){
         if(activityList[i].checked == true){
             count += 1;
-        }
-       
+        }       
     }
     if(count !== 0){
-       allActivities.firstElementChild.classNmae='valid';
+       allActivities.firstElementChild.className='valid';
        allActivities.lastElementChild.style.display='none';
        return true; 
     } else{
@@ -200,6 +197,83 @@ function validateActivity(){
     }
 
 };
+
+// Validate Credit card number, zip code and CVV 
+
+// credit card number should have between 13-16 digits 
+let  creditNumRegex =/^[0-9]{13,16}$/;
+const inputCreditNum = document.getElementById('cc-num');
+
+inputCreditNum.addEventListener("keyup", () => {
+    validateCreditNum();
+});
+function validateCreditNum(){
+    if(inputCreditNum.value && creditNumRegex.test(inputCreditNum.value)){
+        inputCreditNum.parentNode.className='valid';
+        inputCreditNum.parentNode.lastElementChild.style.display = 'none';
+        return true; 
+     } else {
+         if(inputCreditNum.value){
+             inputCreditNum.parentNode.lastElementChild.textContent = 'Credit card number must be between 13 - 16 digits';
+            } else {
+                inputCreditNum.parentNode.lastElementChild.textContent = 'Credit card number is required';
+            }
+         inputCreditNum.parentNode.className='not-valid';
+         inputCreditNum.parentNode.lastElementChild.style.display = 'block';
+         return false; 
+     }
+} 
+
+
+// validate zip code
+let  zipRegex =/^[0-9]{5}$/;
+const inputZip = document.getElementById('zip');
+
+inputZip.addEventListener("keyup", () => {
+    validateZip();
+});
+function validateZip(){
+    if(inputZip.value && zipRegex.test(inputZip.value)){
+        inputZip.parentNode.className='valid';
+        inputZip.parentNode.lastElementChild.style.display = 'none';
+        return true; 
+     } else {
+         if(inputZip.value){
+             inputZip.parentNode.lastElementChild.textContent = 'Zip Code must be 5 digits';
+            } else {
+                inputZip.parentNode.lastElementChild.textContent = 'Zip code is required';
+            }
+         inputZip.parentNode.className='not-valid';
+         inputZip.parentNode.lastElementChild.style.display = 'block';
+         return false; 
+     }
+} 
+
+
+// validate CVV code
+let  cvvRegex =/^[0-9]{3}$/;
+const inputcvv = document.getElementById('cvv');
+
+inputcvv.addEventListener("keyup", () => {
+    validatecvv();
+});
+function validatecvv(){
+    if(inputcvv.value && cvvRegex.test(inputcvv.value)){
+        inputcvv.parentNode.className='valid';
+        inputcvv.parentNode.lastElementChild.style.display = 'none';
+        return true; 
+     } else {
+         if(inputcvv.value){
+             inputcvv.parentNode.lastElementChild.textContent = 'cvv must be 3 digits';
+            } else {
+                inputcvv.parentNode.lastElementChild.textContent = 'cvv code is required';
+            }
+         inputcvv.parentNode.className='not-valid';
+         inputcvv.parentNode.lastElementChild.style.display = 'block';
+         return false; 
+     }
+} 
+
 
 
 //Submit form --- when "Register" button is clicked, display error messages (hint messages) if the required field is empty or invalid.
@@ -219,6 +293,18 @@ registerForm.addEventListener('submit', (e) => {
      //  Activity list
      if (!validateActivity()) {
         e.preventDefault();
+    }  
+    // Credit card number
+    if (!validateCreditNum()) {
+        e.preventDefault();
     }   
-  
+    // zip code
+    if (!validateZip()) {
+        e.preventDefault();
+    }   
+     // cvv code
+     if (!validatecvv()) {
+        e.preventDefault();
+    } 
+   
 });
